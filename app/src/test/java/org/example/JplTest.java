de942c7d056337caf643f8776678d240fa2663aa
lost22git <lost22git>
2022-10-20 00:49:49 +0800

    initial commit

---
+++ app/src/test/java/org/example/JplTest.java
@@ -0,0 +1,83 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.logging.LogManager;
+
+import static java.lang.System.Logger.Level.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Java platform logging api
+ *
+ * @see https://openjdk.org/jeps/264
+ */
+public class JplTest {
+
+
+
+    /**
+     * # Without JUL module installed
+     * <p>
+     * Default {@link System.Logger} impl is {@link jdk.internal.logger.SimpleConsoleLogger}
+     * <p>
+     * Default writer is {@link System.err}
+     * <p>
+     * Default level is {@link System.Logger.Level.INFO}
+     * <p>
+     * Default formatter is
+     * <pre>
+     * String.format(format:String,
+     *                          now:ZoneDateTime,
+     *                          callerInfo:String,
+     *                          name:String,
+     *                          level:String,
+     *                          msg:String,
+     *                          throwable:String);
+     * </pre>
+     * format value get from system property `jdk.system.logger.format`
+     * <p>
+     * and the default value is `%1$tb %1$td, %1$tY %1$tl:%1$tM:%1$tS %1$Tp %2$s%n%4$s: %5$s%6$s%n`
+     * <p>
+     * <p>
+     * # With JUL module installed
+     * <p>
+     * {@link System.Logger} -> {@link java.util.logging.Logger}
+     * <p>
+     * Default {@link System.Logger} impl is {@link LoggingProviderImpl.JULWrapper}
+     * <p>
+     * Default {@link java.lang.System.LoggerFinder} impl is {@link sun.util.logging.LoggingProviderImpl}
+     */
+    @Test
+    void testApi() {
+        var logger = System.getLogger("a.b.c");
+        logger.log(DEBUG, "JPL DEBUG");
+        logger.log(INFO, "JPL INFO");
+        logger.log(WARNING, "JPL WARNING");
+        logger.log(ERROR, "JPL ERROR", new Exception("一个错误"));
+    }
+
+    @Test
+    void testLoggerIsJULWrapper() {
+        var logger = System.getLogger("a.b.c");
+        assertThat(logger.getClass().getName()).contains("JULWrapper");
+    }
+
+    @Test
+    void testCustomJulConfig() throws Exception {
+        // JUL re-read config file from specify path
+        System.setProperty("java.util.logging.config.file", "src/test/resources/logging.properties");
+        var logManager = LogManager.getLogManager();
+        logManager.readConfiguration();
+
+        testApi();
+    }
+
+    @Test
+    void testLoggerIsNotShared() {
+        var logger = System.getLogger("a.b");
+        var logger2 = System.getLogger("a.b.c");
+
+        assertThat(logger).isNotSameAs(logger2);
+    }
+}
---
+++ app/src/test/java/org/example/JulTest.java
@@ -0,0 +1,101 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JulTest {
+
+    @Test
+    void testApi() {
+        var logger = Logger.getLogger("a.b");
+        logger.log(FINEST, "JUL FINEST");
+        logger.log(FINER, "JUL FINER");
+        logger.log(FINE, "JUL FINE");
+        logger.log(CONFIG, "JUL CONFIG");
+        logger.log(INFO, "JUL INFO");
+        logger.log(WARNING, "JUL WARNING");
+        logger.log(SEVERE, "JUL SEVERE", new Exception("一个错误"));
+    }
+
+
+    /**
+     * {@link java.util.logging.LogManager} is a global singleton and responsible for managing configuration, loggers and listeners
+     * <p>
+     * {@link java.util.logging.LogManager.LoggerContext} is a map for managing loggers
+     * <p>
+     * {@link java.util.logging.Logger} is responsible for publishing {@link java.util.logging.LogRecord}
+     * to {@link java.util.logging.Handler}
+     * <p>
+     * {@link java.util.logging.Formatter} is responsible for formatting {@link java.util.logging.LogRecord}
+     */
+    @Test
+    void testCustomConfig() throws IOException {
+        customConfig();
+
+        testApi();
+    }
+
+    static void customConfig() throws IOException {
+        // re-read config file from specify path
+        System.setProperty("java.util.logging.config.file", "src/test/resources/logging.properties");
+        var logManager = LogManager.getLogManager();
+        logManager.readConfiguration();
+    }
+
+    @Test
+    void testLoggerIsNotShared() {
+        var logger = Logger.getLogger("a.b");
+        var logger2 = Logger.getLogger("a.b.c");
+
+        assertThat(logger).isNotSameAs(logger2);
+    }
+
+    @Test
+    void testParentLogger() {
+        var logger = Logger.getLogger("a.b");
+        var logger2 = Logger.getLogger("a.b.c");
+        var rootLogger = Logger.getLogger("");
+
+        assertThat(logger2.getParent()).isSameAs(logger);
+        assertThat(logger.getParent()).isSameAs(rootLogger);
+    }
+
+    @Test
+    void testHandlersIsShared() throws IOException {
+        customConfig();
+
+        var logger = Logger.getLogger("a.b");
+        var logger2 = Logger.getLogger("a.b.c");
+        var rootLogger = Logger.getLogger("");
+
+        var handlers = getHandlersHierarchically(logger);
+        var handlers2 = getHandlersHierarchically(logger2);
+
+//        System.out.println("handlers = " + handlers);
+//        System.out.println("handlers2 = " + handlers2);
+
+        assertThat(handlers).isEqualTo(handlers2);
+        assertThat(List.of(rootLogger.getHandlers())).isEqualTo(handlers2);
+    }
+
+    List<Handler> getHandlersHierarchically(Logger logger) {
+        var list = new ArrayList<Handler>();
+        var l = logger;
+        for (; l != null; ) {
+            list.addAll(Arrays.asList(l.getHandlers()));
+            if (l.getUseParentHandlers()) l = l.getParent();
+            else break;
+        }
+        return list;
+    }
+}
